99860e95eff89cebee74e27c88e8deabf06227f0
AmletixQ <nazarovamridin308@gmail.com>
2025-10-01 23:27:01 +0300

    Configure styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <h1>aaaa</h1>;
+  return (
+    <div>
+      <h1>H1</h1>
+      <h2>H2</h2>
+      <h3>H3</h3>
+      <h4>H4</h4>
+      <h5>H5</h5>
+      <h6>H6</h6>
+
+      <p className="text">base text</p>
+      <p className="text-sm">small text</p>
+
+      <div className="w-10 h-10 bg-red"></div>
+      <div className="w-10 h-10 bg-dark-red"></div>
+      <div className="w-10 h-10 bg-white border"></div>
+      <div className="w-10 h-10 bg-gray"></div>
+      <div className="w-10 h-10 bg-black"></div>
+    </div>
+  );
 }

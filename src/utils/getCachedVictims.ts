d40344686de5d8ed78bd0c5cfffdabd6521fbef4
AmletixQ <nazarovamridin308@gmail.com>
2025-10-04 16:17:55 +0300

    Обновил кэширование с учетом обновления

--- src/utils/getCachedVictims.ts
+++ src/utils/getCachedVictims.ts
@@ -1,29 +1,46 @@
 import { unstable_cache } from "next/cache";
 import prisma from "./prisma";
+import { Prisma } from "@prisma/client";
+import { NATIONALS } from "@/constants/nationals";
 
 export async function getCachedVictims(query: string, filters: string[]) {
   const cacheKey = `victims_${query}_${filters.join("_")}`;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const where: any = {};
+  const where: Prisma.VictimWhereInput = {};
 
   if (query) {
-    where.OR = [
-      { fullname: { contains: query, mode: "insensitive" } },
-      { birthPlace: { contains: query, mode: "insensitive" } },
-      { national: { contains: query, mode: "insensitive" } },
-      { otherData: { contains: query, mode: "insensitive" } },
-    ];
+    where.OR = [{ fullname: { contains: query, mode: "insensitive" } }];
+
+    if (+query) where.OR.push({ birthYear: { equals: parseInt(query) } });
+    if (NATIONALS.includes(query.toLowerCase()))
+      where.OR.push({ national: { contains: query, mode: "insensitive" } });
+
+    where.OR.push({ birthPlace: { contains: query, mode: "insensitive" } });
   }
+
+  console.log(where.OR);
 
   if (filters.length > 0 && !filters.includes("all")) {
     where.category = { in: [] };
-    if (filters.includes("repressed_nat_attribute")) {
-      where.category.in.push("NATATTRIBUTE");
+    console.log("workedQ");
+
+    if (!where.category.in || !Array.isArray(where.category.in)) {
+      console.log("Not array!");
+      return;
     }
-    if (filters.includes("repressed")) {
+
+    if (filters.includes("list-of-itl")) {
       where.category.in.push("REPRESSED");
     }
-    if (filters.includes("dispossessed")) {
+    if (filters.includes("list-of-shooted")) {
+      where.category.in.push("REPRESSED");
+      where.OR?.push({
+        otherData: { contains: "расстрел", mode: "insensitive" },
+      });
+    }
+    if (filters.includes("repressed-nat-attribute")) {
+      where.category.in.push("NATATTRIBUTE");
+    }
+    if (filters.includes("list-of-dispossessed")) {
       where.category.in.push("DISPOSSESSED");
     }
   }

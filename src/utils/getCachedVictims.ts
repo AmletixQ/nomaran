b522569bfdefae7805d1edd9d915ef6d2c96c009
AmletixQ <nazarovamridin308@gmail.com>
2025-10-17 19:20:54 +0300

    Улучшил точность поиска

--- src/utils/getCachedVictims.ts
+++ src/utils/getCachedVictims.ts
@@ -11,6 +11,8 @@ export async function getCachedVictims(
 ) {
   const cacheKey = `victims_${query}_${filters.join("_")}_page_${page}`;
   const whereConditions: Prisma.Sql[] = [];
+  const stringParts: string[] = [];
+  const numberParts: number[] = [];
 
   if (query) {
     const queryParts = query.split(/[\s,]/).filter((q) => q.trim() !== "");
@@ -18,19 +20,39 @@ export async function getCachedVictims(
       const trimmedPart = part.trim();
       const isNumber = !isNaN(Number(trimmedPart)) && trimmedPart !== "";
 
-      if (isNumber) {
-        const year = Number(trimmedPart);
-        whereConditions.push(Prisma.sql`"birth_year" = ${year}`);
-      } else {
-        const trimmedPartReplaced = trimmedPart.replace(/'/g, "''");
-
-        whereConditions.push(Prisma.sql`
-          EXISTS (SELECT 1 from unnest(string_to_array(fullname, ' ')) as word WHERE levenshtein(lower(word), ${trimmedPartReplaced}) <= 2) OR
-          EXISTS (SELECT 1 FROM unnest(string_to_array(lower("birth_place"), ' ')) AS word WHERE levenshtein(word, ${trimmedPart}) <= 2) OR
-          levenshtein(lower(national), ${trimmedPartReplaced}) <= 2
-        `);
-      }
+      if (isNumber) numberParts.push(Number(trimmedPart));
+      else stringParts.push(trimmedPart);
     });
+  }
+
+  if (numberParts.length > 0) {
+    const numberConditions: Prisma.Sql[] = numberParts.map(
+      (year) => Prisma.sql`"birth_year" = ${year}`,
+    );
+    whereConditions.push(
+      Prisma.sql`(${Prisma.join(numberConditions, " OR ")})`,
+    );
+  }
+
+  if (stringParts.length > 0) {
+    const allWords = Prisma.sql`string_to_array(
+      lower("fullname") || ' ' ||
+      COALESCE(lower(birth_place), '') || ' ' || 
+      COALESCE(lower(national), ''), ' '
+    )
+    `;
+
+    whereConditions.push(Prisma.sql`
+      EXISTS (
+        SELECT 1
+        FROM unnest(${allWords}) as word
+        CROSS JOIN (VALUES ${Prisma.join(
+          stringParts.map((p) => Prisma.sql`(${p})`),
+          ", ",
+        )}) as sp(part)
+        WHERE levenshtein(word, sp.part) <= 1
+      )
+    `);
   }
 
   if (filters.length > 0 && !filters.includes("all")) {
@@ -61,11 +83,46 @@ export async function getCachedVictims(
       ? Prisma.sql`WHERE ${Prisma.join(whereConditions, " AND ")}`
       : Prisma.empty;
 
+  let minDistanceClause = Prisma.empty;
+  let orderByClause = Prisma.sql`ORDER BY "id" ASC`;
+  if (stringParts.length > 0) {
+    const allWords = Prisma.sql`string_to_array(
+      lower("fullname") || ' ' ||
+      COALESCE(lower(birth_place), '') || ' ' || 
+      COALESCE(lower(national), ''), ' '
+    )
+    `;
+
+    minDistanceClause = Prisma.sql`, (
+      SELECT MIN(levenshtein(word, sp.part))
+      FROM unnest(${allWords}) as word
+      CROSS JOIN (VALUES ${Prisma.join(
+        stringParts.map((p) => Prisma.sql`(${p})`),
+        ", ",
+      )}) as sp(part)
+    ) as min_distance
+    `;
+
+    orderByClause = Prisma.sql`ORDER BY min_distance ASC, "id" ASC`;
+  }
+
+  console.log(
+    Prisma.sql`
+        SELECT * ${minDistanceClause}
+        FROM "victims"
+        ${whereClause}
+        ${orderByClause}
+        LIMIT ${pageSize} OFFSET ${(page - 1) * pageSize}
+    `,
+  );
+
   const rawVictims = await unstable_cache(
     async () => {
       return prisma.$queryRaw<Victim[]>`
-        SELECT * from victims
+        SELECT * ${minDistanceClause}
+        FROM "victims"
         ${whereClause}
+        ${orderByClause}
         LIMIT ${pageSize} OFFSET ${(page - 1) * pageSize}
       `;
     },
@@ -73,10 +130,11 @@ export async function getCachedVictims(
     { revalidate: 900, tags: ["victims", cacheKey] },
   )();
 
+  // console.log(rawVictims);
   const victims = rawVictims.map(mapRawVictim);
 
   const totalResult = await prisma.$queryRaw<{ count: bigint }[]>`
-    SELECT COUNT(*) as count FROM victims ${whereClause};
+    SELECT COUNT(*) as count FROM victims ${whereClause}
   `;
 
   const total = Number(totalResult[0].count || 0);

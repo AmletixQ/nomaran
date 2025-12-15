d137223f157f0f0f64b0017c0107a17ccfd43214
AmletixQ <nazarovamridin308@gmail.com>
2025-12-15 09:53:16 +0300

    Поменял таблицу

--- src/app/victims/page.tsx
+++ src/app/victims/page.tsx
@@ -47,13 +47,58 @@ export default function VictimsPage() {
               />
             </div>
 
-            <div className="order-3 flex flex-col gap-4 pt-4 h-full">
+            <div className="order-3 flex h-full flex-col gap-4 pt-4">
               <h2 className="text-center text-[16px] uppercase">
                 ИНФОРМАЦИЯ ПО 6-ТИ ТОМАМ КНИГИ ПАМЯТИ ЖЕРТВ НЕЗАКОННЫХ
                 ПОЛИТИЧЕСКИХ РЕПРЕССИЙ 20-30-40- НАЧАЛА 50-х гг. XX ВЕКА ПО
                 СЕВЕРО-ОСЕТИНСКОЙ АССР.
               </h2>
-              <article className="border-gray grid grid-cols-1 h-full gap-1 rounded-md border bg-white p-2">
+              <table className="border-gray mx-2 rounded-sm border text-[16px]">
+                <tbody>
+                  <tr className="border-gray border">
+                    <td valign="top" align="center" className="w-12 hidden md:table-cell">
+                      1
+                    </td>
+                    <td className="border-gray border px-2">
+                      Реабелитировано репрессированных по политическим
+                      обвинениям
+                      <br />
+                      – ИТЛ, тюремные заключения, ссылки
+                      <br />– Высшая мера наказания (расстрел)
+                    </td>
+                    <td valign="bottom" className="w-32 px-2">
+                      5924 чел.
+                      <br />
+                      1656 чел.
+                    </td>
+                  </tr>
+                  <tr className="border-gray border px-2">
+                    <td valign="top" align="center" className="w-12 hidden md:table-cell">
+                      2
+                    </td>
+                    <td className="border-gray border px-2">
+                      Реабелитировано в ходе кампании по
+                      &quot;раскулачиванию&quot;
+                    </td>
+                    <td className="px-2">1710 чел.</td>
+                  </tr>
+                  <tr className="border-gray border">
+                    <td valign="top" align="center" className="w-12 hidden md:table-cell">
+                      3
+                    </td>
+                    <td className="border-gray border px-2">
+                      Репрессировано по национальному признаку
+                    </td>
+                    <td className="px-2">1848 чел.</td>
+                  </tr>
+                  <tr className="border-gray border">
+                    <td className="hidden md:table-cell"></td>
+                    <td className="border-gray border px-2">Всего</td>
+                    <td className="px-2">11138 чел.</td>
+                  </tr>
+                </tbody>
+              </table>
+              {/* <article className="border-gray grid grid-cols-1 h-full gap-1 rounded-md border bg-white p-2">
                 <div className="text-center">
                   <h5 className="text-[16px]">
                     Реабелитировано репрессированных по политическим мотивам
@@ -96,7 +141,7 @@ export default function VictimsPage() {
                   <h5 className="text-[16px]">Итого</h5>
                   <p className="text-[22px]">11138 человек</p>
                 </div>
-              </article>
+              </article> */}
             </div>
           </div>
 
